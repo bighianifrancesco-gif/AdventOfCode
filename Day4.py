303ddb9c3072845742361cc43188e1311bd10528
Francesco Bighiani <bighiani.francesco@gmail.com>
2025-10-31 10:05:31 +0100

    added part 2

--- Day4.py
+++ Day4.py
@@ -14,30 +14,51 @@ grid = [list(line) for line in lines]
 n = len(grid)
 m = len(grid[0])
 
-word = "XMAS"
+# word = "XMAS"
 
-directions = [
-    (-1, -1), (-1, 0), (-1, 1),
-    (0, -1),           (0, 1),
-    (1, -1),  (1, 0),  (1, 1)
-]
+# directions = [
+#     (-1, -1), (-1, 0), (-1, 1),
+#     (0, -1),           (0, 1),
+#     (1, -1),  (1, 0),  (1, 1)
+# ]
+
+# count = 0
+
+# for i in range(n):
+#     for j in range(m):
+#         if grid[i][j] != word[0]:
+#             continue
+#         for di, dj in directions:
+#             x, y = i, j
+#             matched = True
+#             for k in range(1, 4):
+#                 x += di
+#                 y += dj
+#                 if not (0 <= x < n and 0 <= y < m and grid[x][y] == word[k]):
+#                     matched = False
+#                     break
+#             if matched:
+#                 count += 1
+
+# print(count)
 
 count = 0
 
-for i in range(n):
-    for j in range(m):
-        if grid[i][j] != word[0]:
+for i in range(1, n-1):        
+    for j in range(1, m-1):
+        if grid[i][j] != "A":  
             continue
-        for di, dj in directions:
-            x, y = i, j
-            matched = True
-            for k in range(1, 4):
-                x += di
-                y += dj
-                if not (0 <= x < n and 0 <= y < m and grid[x][y] == word[k]):
-                    matched = False
-                    break
-            if matched:
-                count += 1
 
-print(count)
+        tl = grid[i-1][j-1]    
+        br = grid[i+1][j+1]    
+        tr = grid[i-1][j+1]    
+        bl = grid[i+1][j-1]    
+
+        diag1 = (tl == "M" and br == "S") or (tl == "S" and br == "M")
+        diag2 = (tr == "M" and bl == "S") or (tr == "S" and bl == "M")
+
+        if diag1 and diag2:
+            count += 1
+
+print("Total X-MAS occurrences:", count)
+
